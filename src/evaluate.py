fcfd7fe97e169296760adba3963192c2bce7e4c6
Godfred Nii Opintan <godfred.opintan@icloud.com>
2026-07-26 18:22:44 +0000

    - Made changes to working directory to the optuna.db file - Modified error handling when model selection (choosing between 'custom_cnn' and 'transfer'

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,24 +7,25 @@ from src.models.custom_cnn import WasteClassifierCNN
 from src.models.transfer_model import build_transfer_model
 from src.transforms import train_transforms, test_transforms
 
+PROJECT_ROOT = '/kaggle/working/waste_classificator'
 
 def final_evaluation(model_type, data_root, num_classes, device):
     study = optuna.load_study(
-        storage = f"experiments/{model_type}/optuna_study.db",
+        storage = f"sqlite:///{PROJECT_ROOT}/experiments/{model_type}/optuna_study.db",
         study_name = f"{model_type}_study"
     )
     best_params = study.best_params
 
     train_dataset = WasteDataset(data_root, train_transforms(best_params['img_size']), "train")
-    val_dataset = WasteDataset(data_root, test_transforms(['img_size']), "val")
-    test_dataset = WasteDataset(data_root, test_transforms(['img_size']), "test")
+    val_dataset = WasteDataset(data_root, test_transforms(best_params['img_size']), "val")
+    test_dataset = WasteDataset(data_root, test_transforms(best_params['img_size']), "test")
     
-    if model_type == "custom":
+    if model_type == "custom_cnn":
         model = WasteClassifierCNN(num_classes, base_filters=best_params['best_filters'], dropout=best_params['dropout'])
     elif model_type == "transfer":
         model = build_transfer_model(arch=best_params['arch'], num_classes=num_classes, dropout=best_params['dropout'], freeze_backbone=False)
     else:
-        return "No model of that sort. Choose between 'custom' or 'transfer' "
+        raise ValueError(f"Unknown model type: {model_type}. Choose between 'custom_cnn' or 'transfer'.")
 
     return fit(
         model=model,
